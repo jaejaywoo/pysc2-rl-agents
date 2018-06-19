a49f5a3bbf788a7f201e7604f5ecb1ff7338bfd2
jaejaywoo <hjwoo@umich.edu>
2018-06-19 15:56:57 +0000

    Add a line for removing cache files

--- train.sh
+++ train.sh
@@ -1,1 +1,7 @@
-python -m pudb.run run.py collect_mineral_shards --envs 16 --map CollectMineralShards
+#!/bin/bash
+
+# Remove all __pycache__ files
+rm -rf {rl/__pycache__,rl/agents/a2c/__pycache__,rl/networks/__pycache__}
+
+# Run
+python run.py collect_mineral_shards --eval --envs 16 --map CollectMineralShards
