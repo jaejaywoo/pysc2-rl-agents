16bbdb6569ddf98c0cf5c04b167c1885107a2bd7
jaejaywoo <hjwoo@umich.edu>
2018-06-19 02:32:36 +0000

    Automate unzipping SC2 binary and edit bugs

--- setup.sh
+++ setup.sh
@@ -9,34 +9,52 @@ sudo python get-pip.py
 sudo python3 get-pip.py
 
 # Install virtualenv
-pip install --upgrade virtualenv
-pip install virtualenvwrapper
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
 
-echo "\n\n### User specific Configs ###" >> .bashrc
+echo "### User specific Configs ###" >> .bashrc
 echo "export LC_ALL='en_US.UTF-8'" >> .bashrc
-echo "\nexport WORKON_HOME=$HOME/.virtualenvs" >> .bashrc
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> .bashrc
 echo "export PROJECT_HOME=$HOME/Devel" >> .bashrc
-echo "VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> .bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> .bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> .bashrc
+echo "" >> .bashrc
+source ~/.bashrc
 
 # Install StarCraft II API binary
-cd .local/lib
+sudo apt-get install unzip
+sudo apt-get install expect
+mkdir -p .local/{lib,bin} && cd .local/lib
 wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.3.16.1.zip
-unzip SC2.3.16.1.zip && rm SC2.3.16.1.zip
+
+spawn unzip SC2.3.16.1.zip
+expect "password:"
+send "iagreetotheeula\n";
+interact
+rm SC2.3.16.1.zip
 
 cd StarCraftII/Maps
 wget http://blzdistsc2-a.akamaihd.net/MapPacks/Melee.zip
-unzip Melee.zip && rm Melee.zip
+spawn unzip Melee.zip
+expect "password:"
+send "iagreetotheeula\n";
+interact
+rm -rf Melee.zip
 
 wget https://github.com/deepmind/pysc2/releases/download/v1.2/mini_games.zip
-unzip mini_games.zip && rm mini_games.zip
+unzip mini_games.zip && rm -rf mini_games.zip
 
 cd $HOME
-echo "\n# StarCraftII" >> .bashrc
+echo "# StarCraftII path" >> .bashrc
 echo "export PATH=$HOME/.local/lib:$PATH" >> .bashrc
 echo "export SC2PATH=$HOME/.local/lib/StarCraftII" >> .bashrc
 
 # Create virtualenv
 mkvirtualenv --python=python3 pysc2
-pip install pysc2==1.2
-pip install tensorflow
+pip install --user pysc2==1.2
+pip install --user tensorflow
+
+# Install git and tmux
+sudo apt update
+sudo apt install git-all
+sudo apt-get install tmux
