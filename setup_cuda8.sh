a20c902582a351fdd81503998830c392b2f05787
Hyunjae Woo <hjwoo@umich.edu>
2018-06-20 16:03:35 +0000

    Update setup for CUDA 8

--- setup_cuda8.sh
+++ setup_cuda8.sh
@@ -6,7 +6,7 @@ CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
 sudo dpkg -i ${CUDA_REPO_PKG}
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda-8-0
 
 # install cuDNN v6.0
 CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
@@ -18,4 +18,4 @@ sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
 
 # set environment variables
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
