771712da280a588bc0c21cac2e295dec77b4f299
Hyunjae Woo <hjwoo@umich.edu>
2018-06-20 16:58:33 +0000

    Add envrionment variables for CUDA and cuDNN

--- setup_cuda8.sh
+++ setup_cuda8.sh
@@ -19,5 +19,10 @@ sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
 sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
 
 # set environment variables
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+#export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+#export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+echo '' >> .bashrc
+echo '# Set environment variables for CUDA and cuDNN' >> .bashrc
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64' >> .bashrc
+echo 'export CUDA_HOME=/usr/local/cuda' >> .bashrc
+echo '' >> .bashrc
