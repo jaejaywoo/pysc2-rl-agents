faee2e03f0af472193538f12ff68586a1d70840b
Hyunjae Woo <hjwoo@umich.edu>
2018-07-03 03:48:29 +0000

    CUDA 9.2 -> CUDA 9.0

--- setup_cuda9.sh
+++ setup_cuda9.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-echo "Checking for CUDA and installing."
 
-# Check for CUDA and try to install.
+# install CUDA Toolkit v9.0
+# instructions from https://developer.nvidia.com/cuda-downloads (linux -> x86_64 -> Ubuntu -> 16.04 -> deb (network))
 cd $HOME
-if ! dpkg-query -W cuda-9-0; then
-  # The 17.04 installer works with 17.10.
-  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/cuda-repo-ubuntu1704_9.0.176-1_amd64.deb
-  sudo dpkg -i ./cuda-repo-ubuntu1704_9.0.176-1_amd64.deb
-  sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/7fa2af80.pub
-  sudo apt-get update
-  sudo apt-get install cuda-9-0 -y
-fi
-
-# Enable persistence mode
+CUDA_REPO_PKG="cuda-repo-ubuntu1604_9.0.176-1_amd64.deb"
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+sudo dpkg -i ${CUDA_REPO_PKG}
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda-9-0
 sudo nvidia-smi -pm 1
