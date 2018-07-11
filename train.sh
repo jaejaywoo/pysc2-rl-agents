e41b7b641b2f087ce84724d840e1f808384a697d
Hyunjae Woo <hjwoo@umich.edu>
2018-07-11 15:28:02 +0000

    Clean up

--- train.sh
+++ train.sh
@@ -2,13 +2,13 @@
 
 # Remove all __pycache__ files
 rm -rf {rl/__pycache__,rl/agents/a2c/__pycache__,rl/networks/__pycache__}
-export SLACK_API_TOKEN=''
+export SLACK_API_TOKEN='xoxp-275696664358-274748830675-396209502564-4e013c28a4e30d5eaf6580b87ca977fc'
 
 # Run
 #python run.py move_to_beacon                 --save_iters 500 --nchw --envs 16 --map MoveToBeacon
 #python run.py collect_mineral_shards         --save_iters 500 --nchw --envs 16 --map CollectMineralShards
 #python run.py find_and_defeat_zerglings      --save_iters 500 --nchw --envs 16 --map FindAndDefeatZerglings
-python run.py defeat_roaches                   --debug --save_iters 500 --nchw --envs 16 --map DefeatRoaches
+python run.py defeat_roaches                   --debug --lr 0.00095 --save_iters 500 --nchw --envs 16 --map DefeatRoaches
 #python run.py defeat_zerglings_and_banelings  --lr 0.00095 --save_iters 500 --nchw --envs 16 --map DefeatZerglingsAndBanelings
 #python run.py collect_minerals_and_gas       --save_iters 500 --nchw --envs 16 --map CollectMineralsAndGas
 #python run.py build_marines                  --save_iters 500 --nchw --envs 16 --map BuildMarines
