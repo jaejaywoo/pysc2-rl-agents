cadef758068004d9eb2ba0c9b173104812bbbe57
Hyunjae Woo <hjwoo@umich.edu>
2018-07-16 11:59:11 -0400

    Update run.py

--- run.py
+++ run.py
@@ -66,7 +66,7 @@ parser.add_argument('--summary_iters', type=int, default=10,
                     help='record training summary after this many iterations')
 parser.add_argument('--save_iters', type=int, default=200,
                     help='store checkpoint after this many iterations')
-parser.add_argument('--max_to_keep', type=int, default=2,
+parser.add_argument('--max_to_keep', type=int, default=4,
                     help='maximum number of checkpoints to keep before discarding older ones')
 parser.add_argument('--entropy_weight', type=float, default=1e-3,
                     help='weight of entropy loss')
